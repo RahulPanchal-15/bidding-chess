41be7085a6c17ad10f682f52c3718db2c0adf32d
RahulPanchal-15 <rahulkkm151@gmail.com>
2021-08-10 14:54:04 +0530

    Updates

---
+++ precache-manifest.b35372f156f9a593afb012fbb61c45c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efbcee03a61fc97584905c6330d4b048",
+    "url": "/bidding-chess/index.html"
+  },
+  {
+    "revision": "c1c20790e3a6e0daeaa1",
+    "url": "/bidding-chess/static/css/main.926941cf.chunk.css"
+  },
+  {
+    "revision": "6a06de7b2c67409b6f92",
+    "url": "/bidding-chess/static/js/2.70b60da1.chunk.js"
+  },
+  {
+    "revision": "c1c20790e3a6e0daeaa1",
+    "url": "/bidding-chess/static/js/main.4298c31e.chunk.js"
+  },
+  {
+    "revision": "f8effcf92420241075a4",
+    "url": "/bidding-chess/static/js/runtime-main.3076a92e.js"
+  }
+]);
